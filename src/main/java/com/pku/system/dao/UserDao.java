04eb0167a9856e851ff7793f5c76d844e109acbe
JiangDongyu <jdy12301068@gmail.com>
2019-09-02 20:57:38 +0800

    用户管理修改逻辑

--- src/main/java/com/pku/system/dao/UserDao.java
+++ src/main/java/com/pku/system/dao/UserDao.java
@@ -8,22 +8,22 @@ import java.util.Set;
 
 @Mapper
 public interface UserDao{
-    @Select("SELECT u.id,u.username,u.password,r.r_id,r.r_name FROM tb_user u INNER JOIN tb_role r ON u.r_id=r.r_id")
+    @Select("SELECT u.id,u.username,u.password,r.r_id,r.r_name,u.license FROM tb_user u INNER JOIN tb_role r ON u.r_id=r.r_id")
     public List<User> getAllUser();
 
     @Select("select * from tb_user where id = #{id}")
     public User selectById(int id);
 
-    @Select("select u.id, u.username,u.password,r.r_id,r.r_name from tb_user u INNER JOIN tb_role r ON u.r_id=r.r_id where username = #{username}")
+    @Select("select u.id, u.username,u.password,r.r_id,r.r_name,u.license from tb_user u INNER JOIN tb_role r ON u.r_id=r.r_id where username = #{username}")
     public User selectByName(String username);
 
     @Select("select * from tb_user where r_id = #{r_id}")
     public List<User> selectByRId(int r_id);
 
-    @Insert("insert into tb_user (id,username,password,r_id) values (#{id},#{username},#{password},#{r_id})")
+    @Insert("insert into tb_user (id,username,password,r_id,license,create_time,update_time) values (#{id},#{username},#{password},#{r_id},#{license},#{createTime},#{updateTime})")
     public void addUser(User user);
 
-    @Update("update tb_user set username=#{username},password=#{password},r_id=#{r_id} where id=#{id}")
+    @Update("update tb_user set username=#{username},password=#{password},r_id=#{r_id},license=#{license},update_time=#{updateTime} where id=#{id}")
     public void updateUser(User user);
 
     @Delete("delete from tb_user where id=#{id}")
