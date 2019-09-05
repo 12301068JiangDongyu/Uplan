6874ef66543c99e7bcdccec4a617608977db62af
罗夕 <332810362@qq.com>
2019-09-05 09:38:19 +0800

    更新项目进度+更换日期插件

--- frontend/smartC/src/app/common/constant.ts
+++ frontend/smartC/src/app/common/constant.ts
@@ -1,7 +1,7 @@
 export class Constant  {
-	//URL : string = 'http://139.155.5.200:8080/';
+	URL : string = 'http://139.155.5.200:8080/';
 	// URL : string = 'http://169.254.66.232:8080/';
-	URL : string = 'http://100.6.10.171:8080/';
+	// URL : string = 'http://100.6.10.171:8080/';
 	// URL : string = 'http://localhost:8080/';
 	WaitTime : number = 5000;
 	RecordTypeMap = [{key: 1, value: 'fix', name: '维修'},
